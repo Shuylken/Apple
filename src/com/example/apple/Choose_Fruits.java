2b0476ae4944aed4917ef9d0af79e5c1f96e9851
Shuylken <1968966426@qq.com>
2016-09-22 18:14:49 +0800

    Apple:choosefrutis updata

--- src/com/example/apple/Choose_Fruits.java
+++ src/com/example/apple/Choose_Fruits.java
@@ -6,11 +6,11 @@ import java.util.List;
 import java.util.Map;
 
 import android.app.Activity;
-import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
-public class Choose_Fruits extends ListActivity  {
+/*public class Choose_Fruits extends ListActivity  {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,45 +20,58 @@ public class Choose_Fruits extends ListActivity  {
 		SimpleAdapter adapter = new SimpleAdapter(this, getData(), R.layout.fruits_item,
 				new String[] { "img", "title", "info" }, new int[] { R.id.img, R.id.title, R.id.info });
 		setListAdapter(adapter);
+	}*/
+public class Choose_Fruits extends Activity {
+	private ListView lv;
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.choose_fruits);
+		lv = (ListView) findViewById(R.id.listview);
+		SimpleAdapter adapter = new SimpleAdapter(this, getData(), R.layout.fruits_item,
+				new String[] { "img", "title", "info" }, new int[] { R.id.img, R.id.title, R.id.info });
+		// setListAdapter(adapter);
+		lv.setAdapter(adapter);
 	}
+
 	private List<Map<String, Object>> getData() {
-        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
-        Map<String, Object> map = new HashMap<String, Object>();
-        map.put("img", R.drawable.ic_launcher);
-        map.put("title", "小宗");
-        map.put("info", "电台DJ");
-        list.add(map);
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("img", R.drawable.drawable_apple);
+		map.put("title", "苹果");
+		map.put("info", "又大又甜");
+		list.add(map);
 
-        map = new HashMap<String, Object>();
-        map.put("img", R.drawable.ic_launcher);
-        map.put("title", "貂蝉");
-        map.put("info", "四大美女");
-        list.add(map);
+		map = new HashMap<String, Object>();
+		map.put("img", R.drawable.drawable_pear);
+		map.put("title", "梨");
+		map.put("info", "水多");
+		list.add(map);
 
-        map = new HashMap<String, Object>();
-        map.put("img", R.drawable.ic_launcher);
-        map.put("title", "奶茶");
-        map.put("info", "清纯妹妹");
-        list.add(map);
+		map = new HashMap<String, Object>();
+		map.put("img", R.drawable.ic_launcher);
+		map.put("title", "葡萄");
+		map.put("info", "小葡萄");
+		list.add(map);
 
-        map = new HashMap<String, Object>();
-        map.put("img", R.drawable.ic_launcher);
-        map.put("title", "大黄");
-        map.put("info", "是小狗");
-        list.add(map);
+		map = new HashMap<String, Object>();
+		map.put("img", R.drawable.ic_launcher);
+		map.put("title", "桃子");
+		map.put("info", "红");
+		list.add(map);
 
-        map = new HashMap<String, Object>();
-        map.put("img", R.drawable.ic_launcher);
-        map.put("title", "hello");
-        map.put("info", "every thing");
-        list.add(map);
+		map = new HashMap<String, Object>();
+		map.put("img", R.drawable.ic_launcher);
+		map.put("title", "hello");
+		map.put("info", "every thing");
+		list.add(map);
 
-        map = new HashMap<String, Object>();
-        map.put("img", R.drawable.ic_launcher);
-        map.put("title", "world");
-        map.put("info", "hello world");
-        list.add(map);
+		map = new HashMap<String, Object>();
+		map.put("img", R.drawable.ic_launcher);
+		map.put("title", "world");
+		map.put("info", "hello world");
+		list.add(map);
 
-        return list;
-    }
+		return list;
+	}
 }
